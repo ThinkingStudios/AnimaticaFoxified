d8ebca110277c900b0acaf76ace5307224c9cb62
TexBlock <3140846162@qq.com>
2024-09-15 15:26:48 +0800

    port to neoforge 1.21.1

--- src/main/java/io/github/foundationgames/animatica/Animatica.java
+++ src/main/java/io/github/foundationgames/animatica/Animatica.java
@@ -2,25 +2,51 @@ package io.github.foundationgames.animatica;
 
 import io.github.foundationgames.animatica.animation.AnimationLoader;
 import io.github.foundationgames.animatica.config.AnimaticaConfig;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.common.NeoForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.embeddedt.embeddium.api.OptionPageConstructionEvent;
+import org.embeddedt.embeddium.api.options.structure.StandardOptions;
 
-public class Animatica implements ClientModInitializer {
+@Mod(value = Animatica.NAMESPACE, dist = Dist.CLIENT)
+public class Animatica {
     public static final Logger LOG = LogManager.getLogger("Animatica");
     public static final String NAMESPACE = "animatica";
 
-    public static final AnimaticaConfig CONFIG = new AnimaticaConfig();
+    public Animatica(IEventBus modEventBus, ModContainer modContainer) {
+        if (FMLLoader.getDist().isClient()) {
+            NeoForge.EVENT_BUS.addListener(EventPriority.HIGHEST, ClientTickEvent.Pre.class, event -> {
+                AnimationLoader.INSTANCE.tickTextures();
+            });
 
-    @Override
-    public void onInitializeClient() {
-        ClientTickEvents.START_CLIENT_TICK.register(client -> AnimationLoader.INSTANCE.tickTextures());
+            modEventBus.addListener(RegisterClientReloadListenersEvent.class, event -> {
+                event.registerReloadListener(AnimationLoader.INSTANCE);
+            });
 
-        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(AnimationLoader.INSTANCE);
+            modContainer.registerConfig(ModConfig.Type.CLIENT, AnimaticaConfig.SPEC);
+            modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+
+            if (ModList.get().isLoaded("embeddium")) {
+                OptionPageConstructionEvent.BUS.addListener(event -> {
+                    if (event.getId().matches(StandardOptions.Pages.GENERAL)) {
+                        event.addGroup(AnimaticaConfig.EmbeddiumExtended.getAnimatedTextures());
+                    }
+                });
+            }
+        }
     }
 
     public static Identifier id(String path) {
